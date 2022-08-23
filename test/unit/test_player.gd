789bccbf57ff24263e43a8ae76bb7604b20770ac
Michael J. Freidel <32282481+mfreidel@users.noreply.github.com>
2022-08-23 12:19:28 -0500

    Add setup and teardown funcs

--- test/unit/test_player.gd
+++ test/unit/test_player.gd
@@ -1,13 +1,30 @@
 extends GutTest
 
-var player_res = load("res://player/player.gd")
+var script_res = load("res://player/player.gd")
+var scene_res = load("res://player/player.tscn")
 
+var _script = script_res.new()
+var _obj = scene_res.instance()
+
+
+# -- Set-up and tear-down --
+
+func before_all():
+	# Runs once at start
+	add_child(_obj)
+
+
+func after_all():
+	# Runs once at end
+	_obj.free()
+	_script.free()
+
+
+# Tests
 
 func test_can_create_player():
-	var player_obj = player_res.new()
-	assert_not_null(player_obj)
+	assert_not_null(_script)
 
 
 func test_has_shoot_signal():
-	var player_obj = player_res.new()
-	assert_has_signal(player_obj, "shoot")
+	assert_has_signal(_script, "shoot")
